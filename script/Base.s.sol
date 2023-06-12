e526b598bba3900063d6fb5fd86dbae1996af682
aliberkhsl <aliberkhasil@gmail.com>
2023-06-12 12:52:00 +0100

    refactor :deployment script

--- script/Base.s.sol
+++ script/Base.s.sol
@@ -4,9 +4,6 @@ pragma solidity >=0.8.19;
 import { Script } from "forge-std/Script.sol";
 
 abstract contract BaseScript is Script {
-    /// @dev Included to enable compilation of the script without a $MNEMONIC environment variable.
-    string internal constant TEST_MNEMONIC = "test test test test test test test test test test test junk";
-
     /// @dev Needed for the deterministic deployments.
     bytes32 internal constant ZERO_SALT = bytes32(0);
 
@@ -17,8 +14,7 @@ abstract contract BaseScript is Script {
     string internal mnemonic;
 
     constructor() {
-        mnemonic = vm.envOr("MNEMONIC", TEST_MNEMONIC);
-        (deployer,) = deriveRememberKey({ mnemonic: mnemonic, index: 0 });
+        deployer = vm.rememberKey(vm.envUint("DEPLOYER_PKEY"));
     }
 
     modifier broadcaster() {
--- script/Deploy.s.sol
+++ script/Deploy.s.sol
@@ -1,12 +1,14 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity >=0.8.19;
 
-
 import { BaseScript } from "./Base.s.sol";
-
+import { MultiPoolStrategyFactory } from "src/MultiPoolStrategyFactory.sol";
 /// @dev See the Solidity Scripting tutorial: https://book.getfoundry.sh/tutorials/solidity-scripting
+
 contract Deploy is BaseScript {
-    // function run() public broadcaster returns (Foo foo) {
-  
-    // }
+    address MONITOR = address(0); // TODO : set monitor address before deploy
+
+    function run() public broadcaster returns (MultiPoolStrategyFactory factory) {
+        factory = new MultiPoolStrategyFactory(MONITOR);
+    }
 }
