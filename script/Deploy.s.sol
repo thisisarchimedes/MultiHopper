dfce9e335ce974ac1334509c165ba7ec62d12d0a
Carlos Di Matteo <carlosdima97@gmail.com>
2023-06-15 17:55:02 -0500

    added monitor address not set error

--- script/Deploy.s.sol
+++ script/Deploy.s.sol
@@ -3,12 +3,14 @@ pragma solidity >=0.8.19;
 
 import { BaseScript } from "./Base.s.sol";
 import { MultiPoolStrategyFactory } from "src/MultiPoolStrategyFactory.sol";
+
 /// @dev See the Solidity Scripting tutorial: https://book.getfoundry.sh/tutorials/solidity-scripting
 
 contract Deploy is BaseScript {
-    address MONITOR = address(0); // TODO : set monitor address before deploy
+    address MONITOR = 0x026055f2d5e8b7047B438E6e9291bB39325D1d82; // TODO : set monitor address before deploy
 
     function run() public broadcaster returns (MultiPoolStrategyFactory factory) {
+        require(MONITOR != address(0), "Deploy: monitor address not set");
         factory = new MultiPoolStrategyFactory(MONITOR);
     }
 }
