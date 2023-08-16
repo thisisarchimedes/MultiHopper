64f5f1a9be20643f490ee117235c1da1357cac1a
Artjom Galaktionov <artjom.galaktionov@28stone.com>
2023-08-16 17:00:43 +0300

    Change curve interface back

--- src/interfaces/ICurvePool.sol
+++ src/interfaces/ICurvePool.sol
@@ -7,24 +7,12 @@ interface ICurveBasePool {
     function calc_withdraw_one_coin(uint256, int128) external view returns (uint256);
     function calc_withdraw_one_coin(uint256, uint256) external view returns (uint256);
     function coins(uint256 arg0) external view returns (address);
-    function exchange(int128 i, int128 j, uint256 dx, uint256 min_dy) external returns (uint256);
+    function exchange(int128 i, int128 j, uint256 dx, uint256 min_dy) external;
     function get_dy(int128 i, int128 j, uint256 dx) external view returns (uint256);
     function get_dy_underlying(int128 i, int128 j, uint256 dx) external view returns (uint256);
     function get_virtual_price() external view returns (uint256);
-    function remove_liquidity_one_coin(
-        uint256 _token_amount,
-        int128 i,
-        uint256 min_amount
-    )
-        external
-        returns (uint256);
-    function remove_liquidity_one_coin(
-        uint256 _token_amount,
-        uint256 i,
-        uint256 min_amount
-    )
-        external
-        returns (uint256);
+    function remove_liquidity_one_coin(uint256 _token_amount, int128 i, uint256 min_amount) external;
+    function remove_liquidity_one_coin(uint256 _token_amount, uint256 i, uint256 min_amount) external;
 }
 
 interface ICurveMetaPool {
