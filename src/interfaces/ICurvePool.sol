c81b2aa6bc7adba618062a1c74a62eae652edf9e
Artjom Galaktionov <artjom.galaktionov@28stone.com>
2023-08-21 23:29:41 +0300

    Add interface for 3pool

--- src/interfaces/ICurvePool.sol
+++ src/interfaces/ICurvePool.sol
@@ -73,3 +73,7 @@ interface IPoolFactory5 is ICurveBasePool {
 interface IPoolWithEth {
     function exchange(int128 i, int128 j, uint256 dx, uint256 min_dy) external payable;
 }
+
+interface ICurve3Pool {
+    function calc_withdraw_one_coin(uint256 _token_amount, int128 i) external view returns (uint256, uint256);
+}
