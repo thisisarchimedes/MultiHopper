00a8c00b699f6dd4791b99d3c7050404b1c438b2
sacredwx <sacredwx@gmail.com>
2023-09-20 19:15:57 +0300

    [Add] Verbosity to test/get_quote_lifi.py

--- test/get_quote_lifi.py
+++ test/get_quote_lifi.py
@@ -21,11 +21,17 @@ def get_quote(
         "fromAddress": fromAddress
     }
 
-    resp = requests.get(API_URL, params=queryParams).json()
+    resp = requests.get(API_URL, params=queryParams)
+    if resp.status_code != 200:
+        raise Exception(format(f"{resp.text}, code: {resp.status_code}"))
+    
+    resp = resp.json()
+
     data = encode(
         ["uint256", "bytes"], [int(resp["estimate"]["toAmount"]), codecs.decode(
             resp['transactionRequest']['data'][2:], 'hex_codec')]
     ).hex()
+    
     print("0x" + str(data))
 
 
