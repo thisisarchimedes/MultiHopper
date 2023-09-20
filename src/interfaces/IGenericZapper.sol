6f42341d450deada9da2e1f3ca6bd53a282e041e
sacredwx <sacredwx@gmail.com>
2023-09-20 11:24:15 +0300

    [Fix] GenericZapper Interface

---
+++ src/interfaces/IGenericZapper.sol
@@ -0,0 +1,65 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.10;
+
+import { MultiPoolStrategy as IMultiPoolStrategy } from "../MultiPoolStrategy.sol";
+
+interface IGenericZapper {
+    /**
+     * @custom:error Thrown when the strategy is paused.
+     */
+    error StrategyPaused();
+    /**
+     * @custom:error  Thrown when the asset provided does not match the underlying asset.
+     */
+    error StrategyAssetDoesNotMatchUnderlyingAsset();
+    /**
+     * @custom:error Thrown when an empty input is encountered.
+     */
+    error EmptyInput();
+    /**
+     * @custom:error Thrown when provided address is set to the zero address.
+     */
+    error ZeroAddress();
+
+    /**
+     * @dev Deposits asset into the MultiPoolStrategy contract.
+     * @param amount The asset amount user wants to deposit.
+     * @param token The deposited asset address (like: USDT address).
+     * @param receiver The address to receive the shares.
+     * @param strategyAddress The address of the MultiPoolStrategy contract to deposit into.
+     * @return shares The amount of shares received.
+     */
+    function deposit(
+        uint256 amount,
+        address token,
+        address receiver,
+        address strategyAddress,
+        IMultiPoolStrategy.SwapData[] calldata swapData
+    )
+        external
+        returns (uint256 shares);
+
+    /**
+     * @dev Redeems asset from the MultiPoolStrategy contract.
+     * @param sharesAmount The amount of shares to redeem.
+     * @param redeemToken The token address redeem.
+     * @param receiver The address to receive the redeemed asset.
+     * @param strategyAddress The address of the MultiPoolStrategy contract to redeem from.
+     * @return redeemAmount The redeemed amount.
+     */
+    function redeem(
+        uint256 sharesAmount,
+        address redeemToken,
+        address receiver,
+        address strategyAddress,
+        IMultiPoolStrategy.SwapData[] calldata swapData
+    )
+        external
+        returns (uint256 redeemAmount);
+    /**
+     * 1. User sepcifies how many shares they want to redeem (param: sharesAmount)
+     *     2. Call redeem on the strategy, swaping shares with USDC
+     *     3. Swap USDC with USDT  (Curve reverts if we don't get at least minSwapAmount)
+     *     4. Send USDT to user
+     */
+}
