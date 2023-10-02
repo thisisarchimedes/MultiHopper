aa787c28d8f4f216e9cc5ca5ccc758ba6edca473
sacredwx <sacredwx@gmail.com>
2023-10-02 19:55:43 +0300

    [Add] LiFi Integrator

--- test/get_quote_lifi.py
+++ test/get_quote_lifi.py
@@ -14,6 +14,7 @@ def get_quote(
     returnToAmountMin=False,
 ):
     queryParams = {
+        "integrator": "archimedes",
         "fromChain": 1,
         "toChain": 1,
         "fromToken": srcToken,
