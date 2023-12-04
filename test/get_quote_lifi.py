f7889d60a78ad01fb2443a74148363c71cfecc8c
danielj86 <danieljafe@gmail.com>
2023-12-04 20:05:30 +0200

    added slippage to LiFi quote

--- test/get_quote_lifi.py
+++ test/get_quote_lifi.py
@@ -23,6 +23,7 @@ def get_quote(
         "toToken": dstToken,
         "fromAmount": amount,
         "fromAddress": fromAddress,
+        "slippage":0.07
     }
     for retry in range(MAX_RETRIES):
         resp = requests.get(API_URL, params=queryParams)
