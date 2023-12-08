6fd0f76e9e8be0efcd256215ddb9cb9873619cec
tomerXarchimedes <tomer@archimedesfi.com>
2023-12-08 08:49:47 -0500

    Update DeployStrategyWithAuraWeightedAdapter.s.sol

--- script/DeployStrategyWithAuraWeightedAdapter.s.sol
+++ script/DeployStrategyWithAuraWeightedAdapter.s.sol
@@ -3,11 +3,11 @@
 
 pragma solidity >=0.8.19;
 
+import "forge-std/Script.sol";
 import { console2 } from "forge-std/console2.sol";
 import { WETH as IWETH } from "solmate/tokens/WETH.sol";
 import { IERC20 } from "openzeppelin-contracts/token/ERC20/IERC20.sol";
 
-import { BaseScript } from "script/Base.s.sol";
 import { MultiPoolStrategy } from "src/MultiPoolStrategy.sol";
 import { MultiPoolStrategyFactory } from "src/MultiPoolStrategyFactory.sol";
 import { AuraWeightedPoolAdapter } from "src/AuraWeightedPoolAdapter.sol";
@@ -20,7 +20,7 @@ import { AuraWeightedPoolAdapter } from "src/AuraWeightedPoolAdapter.sol";
  *
  */
 
-contract DeployAuraStable is BaseScript {
+contract DeployAuraStable is Script {
     /**
      * @dev Address of the MultiPoolStrategyFactory contract obtained by running factory deployment script.
      */
@@ -31,26 +31,34 @@ contract DeployAuraStable is BaseScript {
     /// POOL CONSTANTS
     bytes32 public constant BALANCER_WEIGHTED_POOL_ID = 0;
     uint256 public constant AURA_PID = 0;
-    string public constant STRATEGY_NAME = "WETH/SYN Strat";
-    string public constant TOKEN_NAME = "WETH/SYN";
-    string public constant SYMBOL = "WETH/SYN";
+
+    string public constant SALT = "A231208";
+    string public constant STRATEGY_NAME = "Aura Guard";
+    string public constant TOKEN_NAME = "psp.WETH:AURA";
+
+    uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY"); // mainnet deployer private key
+
     /**
      * @dev Executes the deployment and configuration of the Aura Stable Pool Strategy.
      * It performs the following steps:
      *
      */
 
-    function run() public broadcaster {
+    function run() public {
         require(FACTORY_ADDRESS != address(0), "Deploy: factory address not set");
         require(AURA_BOOSTER != address(0), "Deploy: Aura booster address not set");
         require(BALANCER_WEIGHTED_POOL_ID != 0, "Deploy: Balancer pool ID not set");
         require(AURA_PID != 0, "Deploy: Aura pid not set");
 
+        vm.startBroadcast(deployerPrivateKey);
+
         MultiPoolStrategyFactory multiPoolStrategyFactory = MultiPoolStrategyFactory(FACTORY_ADDRESS);
         console2.log("MultiPoolStrategyFactory: %s", address(multiPoolStrategyFactory));
+        
         MultiPoolStrategy multiPoolStrategy =
-            MultiPoolStrategy(multiPoolStrategyFactory.createMultiPoolStrategy(UNDERLYING_ASSET, TOKEN_NAME, SYMBOL));
+            MultiPoolStrategy(multiPoolStrategyFactory.createMultiPoolStrategy(address(IERC20(UNDERLYING_ASSET)), STRATEGY_NAME, TOKEN_NAME));
         console2.log("MultiPoolStrategy: %s", address(multiPoolStrategy));
+        
         AuraWeightedPoolAdapter AuraPoolAdapter = AuraWeightedPoolAdapter(
             multiPoolStrategyFactory.createAuraWeightedPoolAdapter(
                 BALANCER_WEIGHTED_POOL_ID, address(multiPoolStrategy), AURA_PID
@@ -65,5 +73,9 @@ contract DeployAuraStable is BaseScript {
             address(multiPoolStrategy),
             address(AuraPoolAdapter)
         );
+
+        console2.log("Deploy: success - Name: %s ; Symbol: %s", multiPoolStrategy.name(), multiPoolStrategy.symbol());
+
+        vm.stopBroadcast();
     }
 }
