fb48cb6d7c371f8e2b813ce5491d2e1bd053f044
aliberkhsl <aliberkhasil@gmail.com>
2024-02-22 19:40:10 +0000

    refactor : uniswapadapter

--- src/UniswapV3Adapter.sol
+++ src/UniswapV3Adapter.sol
@@ -20,8 +20,8 @@ contract UniswapV3Adapter is Initializable, IUniswapV3MintCallback, ERC20Upgrade
     bool public isToken0;
     ISwapRouter public swapRouter;
 
-    int24 public limitLower;
-    int24 public limitUpper;
+    int24 public lowerTick;
+    int24 public upperTick;
     IERC20Metadata public token0;
     IERC20Metadata public token1;
     bool mintCalled;
@@ -45,10 +45,12 @@ contract UniswapV3Adapter is Initializable, IUniswapV3MintCallback, ERC20Upgrade
         address indexed sender, address indexed receiver, address indexed owner, uint256 assets, uint256 shares
     );
 
+    event DoHardWork(uint256 amount0Collected, uint256 amount1Collected, uint256 amount0Fee, uint256 amount1Fee);
+
     function initialize(
         IUniswapV3Pool _pool,
-        int24 _limitLower,
-        int24 _limitUpper,
+        int24 _lowerTick,
+        int24 _upperTick,
         bool _isToken0,
         address _feeRecipient
     )
@@ -60,8 +62,8 @@ contract UniswapV3Adapter is Initializable, IUniswapV3MintCallback, ERC20Upgrade
         pool = _pool;
         token0 = IERC20Metadata(pool.token0());
         token1 = IERC20Metadata(pool.token1());
-        limitLower = _limitLower;
-        limitUpper = _limitUpper;
+        lowerTick = _lowerTick;
+        upperTick = _upperTick;
         isToken0 = _isToken0;
         swapRouter = ISwapRouter(0xE592427A0AEce92De3Edee1F18E0157C05861564);
         poolFee = pool.fee();
@@ -70,20 +72,25 @@ contract UniswapV3Adapter is Initializable, IUniswapV3MintCallback, ERC20Upgrade
         acceptedSlippage = 9950;
     }
 
+    /**
+     * @dev Deposits a specified amount of tokens to the contract.
+     * @param amount The amount of tokens to deposit.
+     * @param receiver The address of the receiver of the deposited tokens.
+     */
     function deposit(uint256 amount, address receiver) external {
         bool _isToken0 = isToken0; // gas saving
-        int24 _limitLower = limitLower;
-        int24 _limitUpper = limitUpper;
+        int24 _lowerTick = lowerTick;
+        int24 _upperTick = upperTick;
         uint256 _amount = amount;
         //calculate shares
-        uint256 shares = _calcShares(amount);
-        _collectFees(_limitLower, _limitUpper);
+        uint256 shares = _calcShares(amount, _isToken0);
+        _collectFees(_lowerTick, _upperTick);
         _isToken0
             ? token0.safeTransferFrom(msg.sender, address(this), _amount)
             : token1.safeTransferFrom(msg.sender, address(this), _amount);
         //swap logic
         uint256 amountToSwap =
-            _calcAmountToSwap(_amount, _limitLower, _limitUpper, currentTick(), token0.decimals(), token1.decimals());
+            _calcAmountToSwap(_amount, _lowerTick, _upperTick, currentTick(), token0.decimals(), token1.decimals());
         uint256 token0BalBefore = token0.balanceOf(address(this));
         uint256 token1BalBefore = token1.balanceOf(address(this));
         if (amountToSwap > 0) {
@@ -94,9 +101,9 @@ contract UniswapV3Adapter is Initializable, IUniswapV3MintCallback, ERC20Upgrade
         _checkReceivedAmount(
             _amount, _isToken0 ? token0BalAfter : token1BalAfter, _isToken0 ? token1BalAfter : token0BalAfter, _isToken0
         );
-        uint128 liquidity = _liquidityForAmounts(_limitLower, _limitUpper, token0BalAfter, token1BalAfter);
-        (uint256 min0Amount, uint256 min1Amount) = _amountsForLiquidity(_limitLower, _limitUpper, liquidity);
-        _mintLiquidity(_limitLower, _limitUpper, liquidity, address(this), min0Amount, min1Amount);
+        uint128 liquidity = _liquidityForAmounts(_lowerTick, _upperTick, token0BalAfter, token1BalAfter);
+        (uint256 min0Amount, uint256 min1Amount) = _amountsForLiquidity(_lowerTick, _upperTick, liquidity);
+        _mintLiquidity(_lowerTick, _upperTick, liquidity, address(this), min0Amount, min1Amount);
         _mint(receiver, shares);
         emit Deposit(msg.sender, receiver, _amount, shares);
     }
@@ -113,27 +120,14 @@ contract UniswapV3Adapter is Initializable, IUniswapV3MintCallback, ERC20Upgrade
         if (owner != msg.sender) {
             _spendAllowance(msg.sender, owner, shares);
         }
-        (int24 _limitLower, int24 _limitUpper, bool _isToken0) = (limitLower, limitUpper, isToken0);
-        _collectFees(_limitLower, _limitUpper);
-        uint256 totalAmountToSend = _calcAssetsAndBurnLiquidity(_limitLower, _limitUpper, shares, _isToken0);
+        (int24 _lowerTick, int24 _upperTick, bool _isToken0) = (lowerTick, upperTick, isToken0);
+        _collectFees(_lowerTick, _upperTick);
+        uint256 totalAmountToSend = _calcAssetsAndBurnLiquidity(_lowerTick, _upperTick, shares, _isToken0);
         _burn(owner, shares);
         if (totalAmountToSend < minimumReceive) revert NotEnoughToken();
         _isToken0 ? token0.safeTransfer(receiver, totalAmountToSend) : token1.safeTransfer(receiver, totalAmountToSend);
         emit Withdraw(msg.sender, receiver, owner, totalAmountToSend, shares);
         return totalAmountToSend;
-    }
-
-    function underlyingBalance() public view returns (uint256) {
-        int24 tick = currentTick();
-
-        (, uint128 amount0, uint128 amount1) = getPosition();
-        uint128 curValBal =
-            isToken0 ? uint128(token1.balanceOf(address(this))) : uint128(token0.balanceOf(address(this)));
-        uint256 amount = isToken0
-            ? tick.getQuoteAtTick(amount1 + curValBal, address(token1), address(token0))
-            : tick.getQuoteAtTick(amount0 + curValBal, address(token0), address(token1));
-        uint256 curUnderlyingBal = isToken0 ? token0.balanceOf(address(this)) : token1.balanceOf(address(this));
-        return isToken0 ? amount0 + amount + curUnderlyingBal : amount1 + amount + curUnderlyingBal;
     }
 
     /// @notice Callback function of uniswapV3Pool mint
@@ -156,8 +150,8 @@ contract UniswapV3Adapter is Initializable, IUniswapV3MintCallback, ERC20Upgrade
     /// @return amount1 Estimated amount of token1 that could be collected by
     /// burning the  position
     function getPosition() public view returns (uint128 liquidity, uint128 amount0, uint128 amount1) {
-        (uint128 positionLiquidity, uint128 tokensOwed0, uint128 tokensOwed1) = _position(limitLower, limitUpper);
-        (uint256 _amount0, uint256 _amount1) = _amountsForLiquidity(limitLower, limitUpper, positionLiquidity);
+        (uint128 positionLiquidity, uint128 tokensOwed0, uint128 tokensOwed1) = _position(lowerTick, upperTick);
+        (uint256 _amount0, uint256 _amount1) = _amountsForLiquidity(lowerTick, upperTick, positionLiquidity);
         amount0 = uint128(_amount0) + (tokensOwed0);
         amount1 = uint128(_amount1) + (tokensOwed1);
         liquidity = positionLiquidity;
@@ -165,21 +159,22 @@ contract UniswapV3Adapter is Initializable, IUniswapV3MintCallback, ERC20Upgrade
 
     /// @notice Compound pending fees
     function doHardWork() external onlyOwner {
-        int24 _limitLower = limitLower;
-        int24 _limitUpper = limitUpper;
-        _collectFees(_limitLower, _limitUpper);
+        int24 _lowerTick = lowerTick;
+        int24 _upperTick = upperTick;
+        (, uint256 amount0Collected, uint256 amount1Collected, uint256 amount0Fee, uint256 amount1Fee) =
+            _collectFees(_lowerTick, _upperTick);
 
         uint128 liquidity = _liquidityForAmounts(
-            _limitLower, _limitUpper, token0.balanceOf(address(this)), token1.balanceOf(address(this))
+            _lowerTick, _upperTick, token0.balanceOf(address(this)), token1.balanceOf(address(this))
         );
-        (uint256 min0Amount, uint256 min1Amount) = _amountsForLiquidity(_limitLower, _limitUpper, liquidity);
-        _mintLiquidity(_limitLower, _limitUpper, liquidity, address(this), min0Amount, min1Amount);
-        // TODO add event
+        (uint256 min0Amount, uint256 min1Amount) = _amountsForLiquidity(_lowerTick, _upperTick, liquidity);
+        _mintLiquidity(_lowerTick, _upperTick, liquidity, address(this), min0Amount, min1Amount);
+        emit DoHardWork(amount0Collected, amount1Collected, amount0Fee, amount1Fee);
     }
 
     function rebalance(
-        int24 _limitLower,
-        int24 _limitUpper,
+        int24 _lowerTick,
+        int24 _upperTick,
         uint256 amount0OutMin,
         uint256 amount1OutMin
     )
@@ -187,29 +182,42 @@ contract UniswapV3Adapter is Initializable, IUniswapV3MintCallback, ERC20Upgrade
         onlyOwner
     {
         int24 tickSpacing = pool.tickSpacing();
-        // _limitLower = _limitLower / tickSpacing * tickSpacing;
-        // _limitUpper = _limitUpper / tickSpacing * tickSpacing;
-        if (_limitLower >= _limitUpper) revert InvalidRange();
-        if (_limitLower % tickSpacing != 0 || _limitUpper % tickSpacing != 0) revert InvalidRange();
-        int24 _currentLimitLower = limitLower;
-        int24 _currentLimitUpper = limitUpper;
-        _collectFees(_currentLimitLower, _currentLimitUpper);
-        (uint128 liquidity,,) = _position(_currentLimitLower, _currentLimitUpper);
+        if (_lowerTick >= _upperTick) revert InvalidRange();
+        if (_lowerTick % tickSpacing != 0 || _upperTick % tickSpacing != 0) revert InvalidRange();
+        int24 _currentlowerTick = lowerTick;
+        int24 _currentupperTick = upperTick;
+        _collectFees(_currentlowerTick, _currentupperTick);
+        (uint128 liquidity,,) = _position(_currentlowerTick, _currentupperTick);
         _burnLiquidity(
-            _currentLimitLower, _currentLimitUpper, liquidity, address(this), true, amount0OutMin, amount1OutMin
+            _currentlowerTick, _currentupperTick, liquidity, address(this), true, amount0OutMin, amount1OutMin
         );
-        limitLower = _limitLower;
-        limitUpper = _limitUpper;
+        lowerTick = _lowerTick;
+        upperTick = _upperTick;
 
         uint128 newLiquidity = _liquidityForAmounts(
-            _limitLower, _limitUpper, token0.balanceOf(address(this)), token1.balanceOf(address(this))
+            _lowerTick, _upperTick, token0.balanceOf(address(this)), token1.balanceOf(address(this))
         );
-        (uint256 min0Amount, uint256 min1Amount) = _amountsForLiquidity(_limitLower, _limitUpper, newLiquidity);
-        _mintLiquidity(_limitLower, _limitUpper, newLiquidity, address(this), min0Amount, min1Amount);
+        (uint256 min0Amount, uint256 min1Amount) = _amountsForLiquidity(_lowerTick, _upperTick, newLiquidity);
+        _mintLiquidity(_lowerTick, _upperTick, newLiquidity, address(this), min0Amount, min1Amount);
         // TODO add event
     }
 
+    function underlyingBalance() external view returns (uint256) {
+        return _underlyingBalance(isToken0);
+    }
     /// INTERNAL FUNCTIONS
+
+    function _underlyingBalance(bool _isToken0) internal view returns (uint256) {
+        int24 tick = currentTick();
+        (, uint128 amount0, uint128 amount1) = getPosition();
+        uint128 curValBal =
+            _isToken0 ? uint128(token1.balanceOf(address(this))) : uint128(token0.balanceOf(address(this)));
+        uint256 amount = _isToken0
+            ? tick.getQuoteAtTick(amount1 + curValBal, address(token1), address(token0))
+            : tick.getQuoteAtTick(amount0 + curValBal, address(token0), address(token1));
+        uint256 curUnderlyingBal = _isToken0 ? token0.balanceOf(address(this)) : token1.balanceOf(address(this));
+        return _isToken0 ? amount0 + amount + curUnderlyingBal : amount1 + amount + curUnderlyingBal;
+    }
 
     function _swapTokens(
         uint256 amountIn,
@@ -237,18 +245,23 @@ contract UniswapV3Adapter is Initializable, IUniswapV3MintCallback, ERC20Upgrade
         );
     }
 
-    function _collectFees(int24 tickLower, int24 tickUpper) internal returns (uint128 liquidity) {
+    function _collectFees(
+        int24 tickLower,
+        int24 tickUpper
+    )
+        internal
+        returns (uint128 liquidity, uint256 owed0, uint256 owed1, uint256 feeAmount0, uint256 feeAmount1)
+    {
         (liquidity,,) = _position(tickLower, tickUpper);
         if (liquidity > 0) {
             pool.burn(tickLower, tickUpper, 0);
-            (uint256 owed0, uint256 owed1) =
-                pool.collect(address(this), tickLower, tickUpper, type(uint128).max, type(uint128).max);
+            (owed0, owed1) = pool.collect(address(this), tickLower, tickUpper, type(uint128).max, type(uint128).max);
             if (owed0 > 0) {
-                uint256 feeAmount0 = (owed0) * fee / BASE;
+                feeAmount0 = (owed0) * fee / BASE;
                 token0.safeTransfer(feeRecipient, feeAmount0);
             }
             if (owed1 > 0) {
-                uint256 feeAmount1 = (owed1) * fee / BASE;
+                feeAmount1 = (owed1) * fee / BASE;
                 token1.safeTransfer(feeRecipient, feeAmount1);
             }
         }
@@ -272,8 +285,8 @@ contract UniswapV3Adapter is Initializable, IUniswapV3MintCallback, ERC20Upgrade
     }
 
     function _calcAssetsAndBurnLiquidity(
-        int24 _limitLower,
-        int24 _limitUpper,
+        int24 _lowerTick,
+        int24 _upperTick,
         uint256 _shares,
         bool _isToken0
     )
@@ -284,9 +297,9 @@ contract UniswapV3Adapter is Initializable, IUniswapV3MintCallback, ERC20Upgrade
         uint256 token1BalBefore = token1.balanceOf(address(this));
         uint256 token0FromBal = token0BalBefore * _shares / totalSupply();
         uint256 token1FromBal = token1BalBefore * _shares / totalSupply();
-        uint128 liqShares = _liquidityForShares(_limitLower, _limitUpper, _shares);
+        uint128 liqShares = _liquidityForShares(_lowerTick, _upperTick, _shares);
 
-        _burnLiquidity(_limitLower, _limitUpper, liqShares, address(this), true, 0, 0);
+        _burnLiquidity(_lowerTick, _upperTick, liqShares, address(this), true, 0, 0);
         uint256 token0After = token0.balanceOf(address(this));
         uint256 token1After = token1.balanceOf(address(this));
         uint256 receivedAmount = _swapTokens(
@@ -328,18 +341,18 @@ contract UniswapV3Adapter is Initializable, IUniswapV3MintCallback, ERC20Upgrade
         );
     }
 
-    function _calcShares(uint256 amount) internal view returns (uint256) {
+    function _calcShares(uint256 amount, bool _isToken0) internal view returns (uint256) {
         uint256 supply = totalSupply();
         if (supply == 0 || amount == 0) {
             return amount;
         }
-        return (amount * supply) / underlyingBalance();
+        return (amount * supply) / _underlyingBalance(_isToken0);
     }
 
     function _calcAmountToSwap(
         uint256 amount,
-        int24 lowerTick,
-        int24 upperTick,
+        int24 _lowerTick,
+        int24 _upperTick,
         int24 _currentTick,
         uint8 token0Decimal,
         uint8 token1Decimal
@@ -348,10 +361,10 @@ contract UniswapV3Adapter is Initializable, IUniswapV3MintCallback, ERC20Upgrade
         view
         returns (uint256)
     {
-        if (_currentTick > upperTick) {
+        if (_currentTick > _upperTick) {
             return isToken0 ? amount : 0;
         }
-        if (_currentTick < lowerTick) {
+        if (_currentTick < _lowerTick) {
             return isToken0 ? 0 : amount;
         }
 
@@ -364,7 +377,7 @@ contract UniswapV3Adapter is Initializable, IUniswapV3MintCallback, ERC20Upgrade
             TickMath.getSqrtRatioAtTick(lowerTick), TickMath.getSqrtRatioAtTick(_currentTick), 10 ** token1Decimal
         );
         uint256 amount0 = LiquidityAmounts.getAmount0ForLiquidity(
-            TickMath.getSqrtRatioAtTick(_currentTick), TickMath.getSqrtRatioAtTick(upperTick), liq
+            TickMath.getSqrtRatioAtTick(_currentTick), TickMath.getSqrtRatioAtTick(_upperTick), liq
         );
         uint256 amount0inToken1 = amount0 * token0InToken1 / 10 ** token0Decimal;
         uint256 token1InToken0;
---
+++ test/uniswap/UniswapV3AdapterRebalanceTest.t.sol
@@ -0,0 +1,74 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.19.0;
+
+import "forge-std/console.sol";
+import "forge-std/StdStorage.sol";
+import { PRBTest } from "@prb/test/PRBTest.sol";
+
+import { console2 } from "forge-std/console2.sol";
+import { StdCheats } from "forge-std/StdCheats.sol";
+import { UniswapV3Adapter } from "src/UniswapV3Adapter.sol";
+import "@uniswap/v3-core/contracts/interfaces/IUniswapV3Pool.sol";
+import "univ3-periphery/libraries/OracleLibrary.sol";
+import { IERC20 } from "openzeppelin-contracts/token/ERC20/IERC20.sol";
+import "univ3-periphery/interfaces/ISwapRouter.sol";
+import "@uniswap/v3-core/contracts/interfaces/IUniswapV3Pool.sol";
+
+contract UniswapV3AdapterRebalanceTest is PRBTest, StdCheats {
+    using OracleLibrary for int24;
+
+    UniswapV3Adapter uniswapV3Adapter;
+    address public constant WETH = 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2;
+    address public constant WBTC = 0x2260FAC5E5542a773Aa44fBCfeDf7C193bc2C599;
+    IUniswapV3Pool public constant WETH_WBTC_POOL = IUniswapV3Pool(0xCBCdF9626bC03E24f779434178A73a0B4bad62eD);
+    ISwapRouter public swapRouter = ISwapRouter(0xE592427A0AEce92De3Edee1F18E0157C05861564);
+    address feeRecipient = makeAddr("feeRecipient");
+
+    function setUp() public virtual {
+        // solhint-disable-previous-line no-empty-blocks
+        string memory alchemyApiKey = vm.envOr("API_KEY_ALCHEMY", string(""));
+        if (bytes(alchemyApiKey).length == 0) {
+            return;
+        }
+
+        // Otherwise, run the test against the mainnet fork.
+        vm.createSelectFork({ urlOrAlias: "mainnet", blockNumber: 19_183_629 });
+
+        uniswapV3Adapter = new UniswapV3Adapter();
+    }
+
+    function testProvideLiquidityInRangeAndRebalance() public {
+        (int24 lowerTick, int24 upperTick,) = chooseTicks(97, 103);
+        uniswapV3Adapter.initialize(WETH_WBTC_POOL, lowerTick, upperTick, false, feeRecipient);
+        _deposit(50e18);
+        (int24 newLowerTick, int24 newUpperTick,) = chooseTicks(95, 105);
+        uniswapV3Adapter.rebalance(newLowerTick, newUpperTick, 0, 0);
+        int24 currentLowerTick = uniswapV3Adapter.lowerTick();
+        int24 currentUpperTick = uniswapV3Adapter.upperTick();
+        assertEq(currentLowerTick, newLowerTick);
+        assertEq(currentUpperTick, newUpperTick);
+        bytes32 positionKey = keccak256(abi.encodePacked(address(uniswapV3Adapter), lowerTick, upperTick));
+        (uint128 liquidity,,,,) = WETH_WBTC_POOL.positions(positionKey);
+        assertEq(liquidity, 0);
+        uint256 underlyingBal = uniswapV3Adapter.underlyingBalance();
+        uint256 expectedError = 50e18 * 2 / 1000;
+        assertAlmostEq(underlyingBal, 50e18, expectedError);
+        positionKey = keccak256(abi.encodePacked(address(uniswapV3Adapter), newLowerTick, newUpperTick));
+        (liquidity,,,,) = WETH_WBTC_POOL.positions(positionKey);
+        assertGt(liquidity, 0);
+    }
+
+    function chooseTicks(int24 lowerPercentile, int24 upperPercentile) public view returns (int24, int24, int24) {
+        (, int24 tick,,,,,) = WETH_WBTC_POOL.slot0();
+        int24 tickSpacing = WETH_WBTC_POOL.tickSpacing();
+        int24 lowerTick = (int24(int128(tick) * lowerPercentile / 100)) / tickSpacing * tickSpacing;
+        int24 upperTick = (int24(int128(tick) * upperPercentile / 100)) / tickSpacing * tickSpacing;
+        return (lowerTick, upperTick, tick);
+    }
+
+    function _deposit(uint256 amount) internal {
+        deal(WETH, address(this), amount);
+        IERC20(WETH).approve(address(uniswapV3Adapter), amount);
+        uniswapV3Adapter.deposit(amount, address(this));
+    }
+}
